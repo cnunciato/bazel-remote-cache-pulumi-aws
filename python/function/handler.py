006200086eaee84d49d5f2d1ae13197d9b23ad28
Christian Nunciato <chris@nunciato.org>
2025-01-13 07:28:30 -0800

    Fix an auth bug in the Py handler

--- python/function/handler.py
+++ python/function/handler.py
@@ -4,18 +4,25 @@ from typing import Dict, Any
 
 def handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     request = event["Records"][0]["cf"]["request"]
-    headers = request.get("headers", {})
 
     custom_headers = request.get("origin")["s3"]["customHeaders"]
     configured_username = custom_headers["x-basic-auth-username"][0]["value"]
     configured_password = custom_headers["x-basic-auth-password"][0]["value"]
 
-    expected_auth = f"Basic {base64.b64encode(f'{configured_username}:{configured_password}'.encode()).decode()}"
-    if (
-        headers.get("authorization")
-        and headers["authorization"][0]["value"] == expected_auth
-    ):
-        return request
+    headers = request.get("headers")
+    auth = headers.get("authorization")
+
+    if auth:
+        decoded = (
+            base64.b64decode(auth[0]["value"].split("Basic ")[1])
+            .decode("utf-8")
+            .split(":")
+        )
+        username = decoded[0]
+        password = decoded[1]
+
+        if username == configured_username and password == configured_password:
+            return request
 
     return {
         "status": "401",
